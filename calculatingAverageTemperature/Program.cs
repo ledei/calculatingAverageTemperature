3ae5b2c7e7bee135e2e2b86776c4e8f2cd930c57
Niklas Nguyen <113009525+ledei@users.noreply.github.com>
2024-08-11 19:17:42 +0200

    Add files via upload

---
+++ calculatingAverageTemperature/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace calculatingAverageTemperature
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] temperatures = {12,16,20,27,28,19};
+
+            PrintAverage(temperatures);
+        }
+
+             static void PrintAverage(double[] temperatures)
+        {
+            double result = CalculateAverage(temperatures);
+            
+            Console.WriteLine($"The average temperature is: {result}");
+        }
+
+     static double CalculateAverage(double[] temperatures)
+        {
+              double sum = 0;
+            foreach(var temperature in temperatures){
+                sum += temperature;
+            }
+            
+            return sum / temperatures.Length;
+        }
+    }
+}
